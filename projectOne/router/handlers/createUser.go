698e2805a48834e0f0200eaa18dd3f277d2336fd
mikhailpachshenko <103926522+mikhailpachshenko@users.noreply.github.com>
2024-04-16 10:36:01 +0500

    Update create User.go
    first error

--- projectOne/router/handlers/createUser.go
+++ projectOne/router/handlers/createUser.go
@@ -14,7 +14,7 @@ func (us *m.UsersStorage) CreateUser(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		content, err := io.ReadAll(r.Body)
 		if err != nil {
-			log.Println("Error:", err)
+			fmt.Println("Current in first Error matched:", err)
 		}
 		defer r.Body.Close()
 
