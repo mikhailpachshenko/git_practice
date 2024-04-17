9c5b61e256e631e3b16898d7038e1489760f3122
mikhailpachshenko <mikhailpachshenko@gmail.com>
2024-04-17 08:51:39 +0500

    modify configuration data base service

--- projectOne/pkg/config/config.go
+++ projectOne/pkg/config/config.go
@@ -13,11 +13,10 @@ const (
 	port     = 5432
 	user     = "mikhailpachshenko"
 	password = "1722"
-	dbname   = "project"
 )
 
 func ConnectionDatabase() *sql.DB {
-	pgSqlConn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	pgSqlConn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=project sslmode=disable", host, port, user, password)
 	db, err := sql.Open("postgres", pgSqlConn)
 	if err != nil {
 		log.Fatalln(err)
