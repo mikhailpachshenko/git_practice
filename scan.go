fe955825526b82e61e2411bb4521bf28baaeb2b1
mikhailpachshenko <mikhailpachshenko@gmail.com>
2024-04-17 08:02:33 +0500

    first commit, includ two functions like scanner

---
+++ scan.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
+
+func strScan(input string) (outStr string) {
+	scn := bufio.NewScanner(os.Stdin)
+
+	for scn.Scan() {
+		outStr = scn.Text()
+		fmt.Println("Out:", outStr)
+	}
+
+	return
+}
+
+func loopInt(input int) (out int) {
+	for {
+		_, err := fmt.Scan(&out)
+		if err == io.EOF {
+			return
+		}
+		fmt.Println("out:", out)
+	}
+	return
+}
