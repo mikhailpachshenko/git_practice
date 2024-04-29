9f5ff99cf0b7ca1312a4601c470b16e7e9fca984
mikhailpachshenko <mikhailpachshenko@gmail.com>
2024-04-29 20:26:28 +0500

    I — merge

--- projectOne/pkg/config/config.go
+++ projectOne/pkg/config/config.go
@@ -37,15 +37,12 @@ func fieldNotNull(input []int) bool {
 	return false
 }
 
-<<<<<<< Updated upstream
 func testField(field []int) {
 	for i := range field {
 		field[i] = i
 	}
 }
 
-=======
->>>>>>> Stashed changes
 func ConnectionDatabase() *sql.DB {
 	pgSqlConn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", pgSqlConn)
